f90a02954ac3dccace17c026b6d05f216b0c62f9
Racsogl06 <racsogl06@gmail.com>
2025-11-28 20:42:59 -0600

    Implementacion de Facturacion Oscar

--- src/main/java/com/luisborrayo/clinicasonrisasana/services/OdontologoService.java
+++ src/main/java/com/luisborrayo/clinicasonrisasana/services/OdontologoService.java
@@ -56,12 +56,13 @@ public class OdontologoService {
 
     public Odontologo obtenerOdontologoPorId(Long id) {
         try {
-            return odontologoRepository.findById(id);
+            return odontologoRepository.findId(id);
         } catch (Exception e) {
             LOGGER.severe("Error al obtener odontólogo por ID: " + id + " - " + e.getMessage());
             return null;
         }
     }
+
 
     public Odontologo obtenerPorColegiado(String colegiado) {
         return odontologoRepository.findByColegiado(colegiado);
